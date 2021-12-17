8348ddaede6b41ad5fa72f92ceb86826a3f81975
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2021-12-17 16:24:29 +0100

    Set up React

---
+++ src/renderer/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div>Renderer</div>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
