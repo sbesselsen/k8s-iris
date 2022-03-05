babf82c41cd043d7510c9a3e3e6e54dda46cdfbb
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2022-03-05 16:45:53 +0100

    Set color theme from context account

---
+++ src/renderer/util/k8s-context-color.ts
@@ -0,0 +1,23 @@
+import seedrandom from "seedrandom";
+import { ColorTheme } from "../context/color-theme";
+
+const themes: ColorTheme[] = [
+    { colorScheme: "gray" },
+    { colorScheme: "red" },
+    { colorScheme: "orange" },
+    { colorScheme: "yellow" },
+    { colorScheme: "green" },
+    { colorScheme: "teal" },
+    { colorScheme: "blue" },
+    { colorScheme: "cyan" },
+    { colorScheme: "purple" },
+    { colorScheme: "pink" },
+];
+
+export const k8sAccountIdColor = (accountId: string | null): ColorTheme => {
+    if (accountId === null) {
+        return themes[0];
+    }
+    const rand = seedrandom(accountId);
+    return themes[Math.floor(rand() * themes.length)];
+};
