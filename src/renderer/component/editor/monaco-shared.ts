d86065cf2925173f94764595921019f9047cb26c
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2022-05-22 16:41:34 +0200

    Fix monaco web worker issue in prod mode

--- src/renderer/component/editor/monaco-shared.ts
+++ src/renderer/component/editor/monaco-shared.ts
@@ -1,9 +1,9 @@
 import { editor } from "monaco-editor";
-import JSONWorker from "url:monaco-editor/esm/vs/language/json/json.worker.js";
-import CSSWorker from "url:monaco-editor/esm/vs/language/css/css.worker.js";
-import HTMLWorker from "url:monaco-editor/esm/vs/language/html/html.worker.js";
-import TSWorker from "url:monaco-editor/esm/vs/language/typescript/ts.worker.js";
-import EditorWorker from "url:monaco-editor/esm/vs/editor/editor.worker.js";
+// import JSONWorker from "url:monaco-editor/esm/vs/language/json/json.worker.js";
+// import CSSWorker from "url:monaco-editor/esm/vs/language/css/css.worker.js";
+// import HTMLWorker from "url:monaco-editor/esm/vs/language/html/html.worker.js";
+// import TSWorker from "url:monaco-editor/esm/vs/language/typescript/ts.worker.js";
+// import EditorWorker from "url:monaco-editor/esm/vs/editor/editor.worker.js";
 
 let initialized = false;
 
@@ -29,24 +29,35 @@ export function initializeMonaco() {
     });
 
     (self as any).MonacoEnvironment = {
-        getWorkerUrl: function (_moduleId, label) {
-            if (label === "json") {
-                return JSONWorker;
+        getWorker: function (_moduleId, label) {
+            // if (label === "json") {
+            //     return JSONWorker;
+            // }
+            // if (label === "css" || label === "scss" || label === "less") {
+            //     return CSSWorker;
+            // }
+            // if (
+            //     label === "html" ||
+            //     label === "handlebars" ||
+            //     label === "razor"
+            // ) {
+            //     return HTMLWorker;
+            // }
+            // if (label === "typescript" || label === "javascript") {
+            //     return TSWorker;
+            // }
+            if (label === "editorWorkerService") {
+                return new Worker(
+                    new URL(
+                        "~/node_modules/monaco-editor/esm/vs/editor/editor.worker.js",
+                        import.meta.url
+                    ),
+                    {
+                        name: label,
+                        type: "module",
+                    }
+                );
             }
-            if (label === "css" || label === "scss" || label === "less") {
-                return CSSWorker;
-            }
-            if (
-                label === "html" ||
-                label === "handlebars" ||
-                label === "razor"
-            ) {
-                return HTMLWorker;
-            }
-            if (label === "typescript" || label === "javascript") {
-                return TSWorker;
-            }
-            return EditorWorker;
         },
     };
 }
