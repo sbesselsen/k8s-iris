6b65cb1a79247ff95f26d4b9c4ca0425ce35d82d
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2023-01-14 19:58:55 +0100

    Add a resource context to simplify resource representation

---
+++ src/renderer/context/k8s-resource.ts
@@ -0,0 +1,128 @@
+import React, {
+    createElement,
+    Fragment,
+    PropsWithChildren,
+    ReactNode,
+    useCallback,
+    useState,
+} from "react";
+import { K8sObject, K8sObjectIdentifier } from "../../common/k8s/client";
+import { useK8sListWatchListener } from "../k8s/list-watch";
+import { create, createStore, transformUseStoreValue } from "../util/state";
+
+type ResourceStoreValue = {
+    resource: K8sObject | undefined;
+    error: any | undefined;
+    isLoading: boolean;
+};
+
+const loadingStoreValue: ResourceStoreValue = {
+    resource: undefined,
+    error: undefined,
+    isLoading: true,
+};
+
+const { useStore, useStoreValue, useStoreValueGetter } =
+    create<ResourceStoreValue>(loadingStoreValue);
+
+export const useResourceValue = transformUseStoreValue(useStoreValue, (v) => {
+    if (!v.resource) {
+        throw new Error(
+            "Cannot call useResoureValue() if no resource is provided"
+        );
+    }
+    return v.resource;
+});
+
+export const useResourceGetter: () => () => K8sObject = () => {
+    const getter = useStoreValueGetter();
+    return useCallback(() => {
+        const { resource } = getter();
+        if (!resource) {
+            throw new Error(
+                "Cannot call useResourceGetter() if no resource is provided"
+            );
+        }
+        return resource;
+    }, [getter]);
+};
+
+export type ResourceContextProps = PropsWithChildren<{
+    objectIdentifier: K8sObjectIdentifier;
+    renderError?: (error: any) => ReactNode;
+    renderLoading?: () => ReactNode;
+    renderNotFound?: () => ReactNode;
+}>;
+
+export const ResourceContext: React.FC<ResourceContextProps> = (props) => {
+    const {
+        objectIdentifier,
+        renderError,
+        renderLoading,
+        renderNotFound,
+        children,
+    } = props;
+    const [store] = useState(() =>
+        createStore<ResourceStoreValue>(loadingStoreValue)
+    );
+
+    const { apiVersion, kind, namespace, name } = objectIdentifier;
+
+    useK8sListWatchListener(
+        {
+            apiVersion: apiVersion,
+            kind: kind,
+            ...(namespace ? { namespaces: [namespace] } : {}),
+            fieldSelector: [{ name: "metadata.name", value: name }],
+        },
+        {
+            onUpdate(message) {
+                const resource: K8sObject | undefined = message.list.items[0];
+                store.set({ resource, error: undefined, isLoading: false });
+            },
+            onWatchError(error) {
+                store.set({ resource: undefined, error, isLoading: false });
+            },
+        },
+        [apiVersion, kind, namespace, name, store]
+    );
+
+    return createElement(
+        useStore.Context.Provider,
+        { value: store },
+        createElement(
+            ResourceContextInner,
+            { renderError, renderLoading, renderNotFound },
+            children
+        )
+    );
+};
+
+type ResourceContextInnerProps = PropsWithChildren<{
+    renderError?: (error: any) => ReactNode;
+    renderLoading?: () => ReactNode;
+    renderNotFound?: () => ReactNode;
+}>;
+
+const renderEmpty = () => null;
+const ResourceContextInner: React.FC<ResourceContextInnerProps> = (props) => {
+    const {
+        renderError = renderEmpty,
+        renderLoading = renderEmpty,
+        renderNotFound = renderEmpty,
+        children,
+    } = props;
+    const isLoading = useStoreValue((v) => v.isLoading);
+    const error = useStoreValue((v) => v.error);
+    const hasResource = useStoreValue((v) => !!v.resource);
+    if (isLoading) {
+        return createElement(Fragment, {}, renderLoading());
+    }
+    if (error) {
+        return createElement(Fragment, {}, renderError(error));
+    }
+    if (!hasResource) {
+        return createElement(Fragment, {}, renderNotFound());
+    }
+    return createElement(Fragment, {}, children);
+};
