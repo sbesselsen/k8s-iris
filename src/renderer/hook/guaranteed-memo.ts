483bbc0f2c7f403bfdb208dae6025ad19fc4f94d
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2023-01-21 14:09:24 +0100

    Add useGuaranteedMemo hook

---
+++ src/renderer/hook/guaranteed-memo.ts
@@ -0,0 +1,21 @@
+import { useRef } from "react";
+
+/**
+ * Works just like useMemo, but actually guarantees that it will keep the value until the deps change.
+ */
+export function useGuaranteedMemo<T>(value: () => T, deps: any[]): T {
+    const valueRef = useRef<T>();
+    const depsRef = useRef<any[]>(deps);
+    if (valueRef.current === undefined) {
+        console.log("init value");
+        valueRef.current = value();
+    } else if (
+        deps.length !== depsRef.current.length ||
+        !deps.every((v, i) => depsRef.current[i] === v)
+    ) {
+        console.log("deps changed");
+        depsRef.current = deps;
+        valueRef.current = value();
+    }
+    return valueRef.current;
+}
