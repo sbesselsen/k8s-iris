7ee36d9d8a70d17b1c16e793739bb7afc8813c96
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2023-05-01 16:46:50 +0200

    Remove Add button, hide toolbar as long as nothing is selected

--- src/renderer/container/resources/ResourcesToolbar.tsx
+++ src/renderer/container/resources/ResourcesToolbar.tsx
@@ -1,15 +1,11 @@
 import { AddIcon } from "@chakra-ui/icons";
-import { Button } from "@chakra-ui/react";
 import React, { useCallback } from "react";
 import {
     K8sObject,
     K8sResourceTypeIdentifier,
 } from "../../../common/k8s/client";
 import { Toolbar } from "../../component/main/Toolbar";
-import { newResourceEditor, useAppEditorsStore } from "../../context/editors";
-import { useAppRouteGetter, useAppRouteSetter } from "../../context/route";
-import { useIpcCall } from "../../hook/ipc";
-import { useKeyListener, useModifierKeyRef } from "../../hook/keyboard";
+import { useKeyListener } from "../../hook/keyboard";
 import { useK8sDeleteAction } from "../../k8s/actions";
 import { ResourceActionButtons } from "./ResourceActionButtons";
 
@@ -20,50 +16,7 @@ export type ResourcesToolbarProps = {
 };
 
 export const ResourcesToolbar: React.FC<ResourcesToolbarProps> = (props) => {
-    const { onClearSelection, resourceType, resources = [] } = props;
-
-    const getAppRoute = useAppRouteGetter();
-    const setAppRoute = useAppRouteSetter();
-    const appEditorsStore = useAppEditorsStore();
-
-    const createWindow = useIpcCall((ipc) => ipc.app.createWindow);
-    const altKeyRef = useModifierKeyRef("Alt");
-    const metaKeyRef = useModifierKeyRef("Meta");
-    const shiftKeyRef = useModifierKeyRef("Shift");
-
-    const onClickAddNew = useCallback(
-        (supportNewWindow = true) => {
-            const editor = newResourceEditor(resourceType);
-            if (supportNewWindow && metaKeyRef.current) {
-                createWindow({
-                    route: {
-                        ...getAppRoute(),
-                        activeEditor: editor,
-                        isSidebarVisible: false,
-                    },
-                });
-            } else if (altKeyRef.current) {
-                // Option+click: open in background.
-                appEditorsStore.set((editors) => {
-                    return [...editors, editor];
-                });
-            } else {
-                setAppRoute((route) => ({
-                    ...route,
-                    activeEditor: editor,
-                }));
-            }
-        },
-        [
-            appEditorsStore,
-            createWindow,
-            getAppRoute,
-            setAppRoute,
-            altKeyRef,
-            metaKeyRef,
-            resourceType,
-        ]
-    );
+    const { onClearSelection, resources = [] } = props;
 
     const deleteResource = useK8sDeleteAction();
 
@@ -77,32 +30,23 @@ export const ResourcesToolbar: React.FC<ResourcesToolbarProps> = (props) => {
     useKeyListener(
         useCallback(
             (event, key) => {
-                if (
-                    event === "keydown" &&
-                    key === "n" &&
-                    metaKeyRef.current &&
-                    !shiftKeyRef.current
-                ) {
-                    onClickAddNew(false);
-                }
                 if (event === "keydown" && key === "Delete") {
                     if (resources?.length > 0) {
                         onClickDelete();
                     }
                 }
             },
-            [onClickAddNew, onClickDelete, metaKeyRef, shiftKeyRef, resources]
+            [onClickDelete, resources]
         )
     );
 
+    if (resources.length === 0) {
+        return null;
+    }
+
     return (
         <Toolbar>
-            <Button onClick={onClickAddNew} leftIcon={<AddIcon w={2} h={2} />}>
-                New
-            </Button>
-            {resources.length > 0 && (
-                <ResourceActionButtons resources={resources} />
-            )}
+            <ResourceActionButtons resources={resources} />
         </Toolbar>
     );
 };
