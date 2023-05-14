66e5479ca083b36bb8ecc684169c752199e66f54
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2023-05-14 12:16:22 +0200

    Auto-update prefs, state, tempData if file is edited externally

--- src/backend/kv/index.ts
+++ src/backend/kv/index.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 import { coalesce } from "../../common/util/async";
+import { deepEqual } from "../../common/util/deep-equal";
 
 export type KvDiskStoreOptions = {
     storageFilePath: string;
@@ -32,27 +33,80 @@ export function createKvDiskStore(options: KvDiskStoreOptions): KvDiskStore {
     const subscribers: Record<string, Array<(newValue: unknown) => void>> = {};
 
     // Load data.
-    if (fs.existsSync(storageFilePath)) {
-        let data = "{}";
-        try {
-            data = fs.readFileSync(storageFilePath, "utf8");
-        } catch (e) {
-            console.error(`Error reading KV file at ${storageFilePath}: ${e}`);
-        }
-        try {
-            values = JSON.parse(data);
-        } catch (e) {
-            console.error(`Error parsing KV file at ${storageFilePath}: ${e}`);
+    function loadData() {
+        if (fs.existsSync(storageFilePath)) {
+            console.log("Read KV", storageFilePath);
+
+            let data = "{}";
+            try {
+                data = fs.readFileSync(storageFilePath, "utf8");
+            } catch (e) {
+                console.error(
+                    `Error reading KV file at ${storageFilePath}: ${e}`
+                );
+            }
+            try {
+                values = JSON.parse(data);
+            } catch (e) {
+                console.error(
+                    `Error parsing KV file at ${storageFilePath}: ${e}`
+                );
+            }
         }
     }
+    loadData();
+
+    let hasChangeWatch = false;
+    let isWriting = false;
+    function watchForChanges() {
+        if (hasChangeWatch) {
+            return;
+        }
+        if (!fs.existsSync(storageFilePath)) {
+            return;
+        }
+        hasChangeWatch = true;
+        fs.watch(storageFilePath, null, (eventType) => {
+            if (isWriting) {
+                // Do not respond to our own writes.
+                return;
+            }
+            if (eventType !== "change") {
+                return;
+            }
+            const prevValues = values;
+
+            loadData();
+
+            // Notify subscribers if keys have changed.
+            for (const [k, v] of Object.entries(values)) {
+                if (!deepEqual(v, prevValues[k])) {
+                    // Value has changed.
+                    subscribers[k]?.forEach((s) => s(v));
+                }
+            }
+            for (const k of Object.keys(prevValues)) {
+                if (!(k in values)) {
+                    // Value has been deleted.
+                    subscribers[k]?.forEach((s) => s(undefined));
+                }
+            }
+        });
+    }
+    watchForChanges();
 
     const commit = coalesce(() => {
         // Commit data synchronously to prevent race conditions.
         console.log("Write KV", storageFilePath);
+        isWriting = true;
         fs.writeFileSync(
             storageFilePath,
             JSON.stringify(values, undefined, "  ")
         );
+        isWriting = false;
+
+        // Create a change watcher if we don't have one already (if the file didnt' exist before this write).
+        watchForChanges();
     }, Math.max(0, writeMaxDelayMs));
 
     return {
